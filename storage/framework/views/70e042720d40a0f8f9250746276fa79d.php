f44da24f08245c4a25447e4c73964c5d4e4e12b0
udinesia325 <udinesiae325@gmail.com>
2023-02-23 09:46:21 +0700

    upgrade to 10

---
+++ storage/framework/views/70e042720d40a0f8f9250746276fa79d.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>" data-theme="mytheme">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title inertia><?php echo e(config('app.name', 'Laravel')); ?></title>
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    <!-- Fonts -->
+    
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/1d385cb44e.js" crossOrigin="anonymous" defer></script>
+    <!-- Scripts -->
+    <?php echo app('Tightenco\Ziggy\BladeRouteGenerator')->generate(); ?>
+    <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?>
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"]); ?>
+    <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->head; } ?>
+</head>
+
+<body class="font-sans antialiased tracking-wide">
+    <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->body; } else { ?><div id="app" data-page="<?php echo e(json_encode($page)); ?>"></div><?php } ?>
+</body>
+
+</html>
+<?php /**PATH C:\Users\SERVERSMK\Documents\projek kelola nilai\kelola_nilai\resources\views/app.blade.php ENDPATH**/ ?>
